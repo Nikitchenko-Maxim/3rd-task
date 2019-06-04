21f192fe619a1bd1825af0c9059e299b2d923f57
Nikitchenko <logadota848@gmail.com>
2019-06-04 15:22:43 +0500

    Added new line referencing to 2nd git branch

--- 3rd task/maxdll.cpp
+++ 3rd task/maxdll.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int MyName(){
 	cout << "Nikitchenko Maksim RI-541216" << endl;
-    cout << "GIT testing" << endl;
-	return 1;
+	cout << "GIT testing" << endl;
+    cout << "New branch to merge after" << endl;
+		return 1;
 }
