1a4241b60c3503c5bd54aca440225c37fd22feaa
Nikitchenko <logadota848@gmail.com>
2019-06-04 14:59:41 +0500

    dll

---
+++ 3rd task/maxdll.cpp
@@ -0,0 +1,12 @@
+#pragma hdrstop
+#pragma argsused
+
+#include <iostream>
+#include "maxdll.h"
+
+using namespace std;
+
+int MyName(){
+	cout << "Nikitchenko Maksim RI-541216" << endl;
+	return 1;
+}
